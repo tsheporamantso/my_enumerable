9da807d0a7e5b8c98299f1d5a9bd43ad986e1ddd
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-24 11:28:19 +0200

    Fix rubocop errors

--- my_list_class.rb
+++ my_list_class.rb
@@ -1,14 +1,15 @@
 require_relative 'my_enumarable_module'
+
 class MyList
-    include MyEnumerable 
+  include MyEnumerable
 
-    def initialize(list)
-        @list = list
-    end
+  def initialize(list)
+    @list = list
+  end
 
-    def each(&block)
-        return enum_for(:each) unless block_given?
+  def each(&block)
+    return enum_for(:each) unless block_given?
 
-        @list.each(&block)
-    end
+    @list.each(&block)
+  end
 end
