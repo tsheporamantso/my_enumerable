6ac4f452129ff69f820bb1acbe44378b94e45557
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-24 11:12:12 +0200

    Created a class mylist with instance variable and implememnted a method that yields members of instance variable

---
+++ my_list_class.rb
@@ -0,0 +1,13 @@
+class MyList
+    include MyEnumerable 
+
+    def initialize(list)
+        @list = list
+    end
+
+    def each(&block)
+        return enum_for(:each) unless block_given?
+
+        @list.each(&block)
+    end
+end
